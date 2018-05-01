0cd76e01fe1444977327c0efca81783d1f2d96c9
Rafael Rezende Costa <rafaelrezendecosta@gmail.com>
2018-04-30 22:10:59 -0300

    Adds Semantic UI theme

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :originubs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #  root to: 'originub#index'
 end
